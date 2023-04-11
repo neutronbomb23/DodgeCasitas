e2da069532f15ac176006833a87d84a905c047ce
PabloM <pabmar31@ucm.es>
2023-04-11 15:21:07 +0200

    TUUU LA CHICA DEAYEEER

--- src/components/ScreenBounce.h
+++ src/components/ScreenBounce.h
@@ -17,10 +17,9 @@ public:
 	}
 
 	void update() {
-		if (tr->getPos().getY() >= ofGetHeight() - tr->getH()) { collisionVector = Vector2D(0, -1); }
-		else if (tr->getPos().getY() <= 0) { collisionVector = Vector2D(0, 1); }
+		if (tr->getPos().getY() >= ofGetHeight() - tr->getH()/2) { collisionVector = Vector2D(0, -1); }
+		else if ( tr->getPos().getY() <= tr->getH() / 2) { collisionVector = Vector2D(0, 1); }
 		else { collisionVector = Vector2D(0, 0); }
 		tr->setVel(tr->getVel() - (collisionVector * ((tr->getVel() * collisionVector) * 2)));
-		//cout << tr->getPos().getX() << " " << tr->getPos().getY() << endl;
 	}
 };
